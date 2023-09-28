1067d1175acc4b1295e666c5f3f6c21a06e1bbd5
RuslanV93 <ruslanvak93@gmail.com>
2023-09-28 20:48:46 +0300

    spend component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import CostItem from './components/CostItem';
+import Costs from './components/Costs';
+
 function App() {
+  const costs = [
+    {
+      date: new Date(2021, 2, 12),
+      description: 'Холодильник',
+      amount: 999.99,
+    },
+    {
+      date: new Date(2021, 6, 25),
+      description: 'Macbook',
+      amount: 1799.99,
+    },
+    {
+      date: new Date(2021, 8, 1),
+      description: 'Джинсы',
+      amount: 47.99,
+    },
+  ];
   return (
     <div>
       <h1>React Bookkeeper</h1>
-      <CostItem></CostItem>
+      <Costs costs={costs} />
     </div>
   );
 }
--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,5 +1,16 @@
-function CostItem() {
-  return <h2>Элемент расхода</h2>;
+import './Costitem.css';
+import CostDate from './CostDate';
+
+function CostItem(props) {
+  return (
+    <div className="cost-item">
+      <CostDate date={props.date} />
+      <div className="cost-item__description">
+        <h2>{props.description}</h2>
+        <div className="cost-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default CostItem;
